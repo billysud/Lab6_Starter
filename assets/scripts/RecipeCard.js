c802defaee37c8775f52ee4370ef2348420aec9a
Billy <bsudirdja@ucsd.edu>
2022-11-02 23:58:15 -0700

    finished part 1

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -124,32 +124,34 @@ class RecipeCard extends HTMLElement {
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
-    let art = this.getAttribute("elementArticle");
-    console.log(art);
+    let art = this.shadowRoot.querySelector("article");
+    console.log(data);
+    
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (tempalte strings) and element.innerHTML for this.
 
-/*
+
     art.innerHTML = `
-      <img src="data.imgSrc"
-      alt="data.imgAlt">
+      <img src="` + data.imgSrc+ `"
+      alt="`+data.imgAlt+`">
     <p class="title">
-      <a href="data.titleLnk">data.titleTxt</a>
+      <a href="`+data.titleLnk+`">`+data.titleTxt+`</a>
     </p>
-    <p class="organization">data.organiation</p>
+    <p class="organization">`+data.organization+`</p>
     <div class="rating">
-      <span>data.numRatings</span>
-      <img src="/assets/images/icons/5-star.svg" alt="5 stars">
-      <span>(500)</span>
+      <span>`+data.rating+`</span>
+      <img src="/assets/images/icons/`+data.rating+`-star.svg" alt="`+data.rating+` stars">
+      <span>`+data.numRatings+`</span>
     </div>
-    <time>50 min</time>
+    <time>`+data.lengthTime+`</time>
     <p class="ingredients">
-      data.ingredients
+      `+data.ingredients+`
     </p>
     `
-    */
+    //console.log(art);
+    
   }
 }
 
